6e20689f7c87e19e0c5f17ced689066b5d64e415
spthiru <pthirumoorthy02062000@gmail.com>
2024-02-23 16:27:52 +0530

    line_tracker_sensor code

---
+++ line_track/baseconfig.h
@@ -0,0 +1,10 @@
+//rcc configuration
+volatile unsigned int *RCC_RC       = (volatile unsigned int *) (0x40023800);
+volatile unsigned int *RCC_CFGR     = (volatile unsigned int *) (0x40023808);
+volatile unsigned int *RCC_AHB1ENR  = (volatile unsigned int *) (0x40023830);
+
+//general purpose input output base address
+volatile unsigned int *GPIOA_MODER  = (volatile unsigned int *) (0x40020000);
+volatile unsigned int *GPIOA_ODR    = (volatile unsigned int *) (0x40020014);
+volatile unsigned int *GPIOA_IDR    = (volatile unsigned int *) (0x40020010);
+volatile unsigned int *GPIOA_PUPDR  = (volatile unsigned int *) (0x4002000c);
---
+++ line_track/main.c
@@ -0,0 +1,70 @@
+#include "baseconfig.h"
+void rcc_config(void)
+{
+   //external clock on clear bit
+   *RCC_RC &= ~(1<<16);
+
+   //external clock on set bit
+   *RCC_RC |= (1<<16);
+
+   //checking external clock on bit or not
+   while((*RCC_RC & (1<<17)) == 0);
+
+   //switch selection clear bit
+	   *RCC_CFGR &= ~(0x3<<0);
+
+   //swtich selection set bit
+   *RCC_CFGR |= (1<<0);
+
+   //checking of switch selection set or not
+   while(!(*RCC_CFGR & 1<<2) == 4);
+
+   //pin selection
+   *RCC_AHB1ENR |= (1<<0);
+}
+void gpio_config(void)
+{
+   //gpio moder selection a0 pin set as output
+   *GPIOA_MODER |= (1<<0);
+
+   //gpio moder selection a1 pin set as input
+   *GPIOA_MODER &= ~(0xc<<0);
+
+   //give a pull down for a1 input pin
+   *GPIOA_PUPDR |= (1<<3);   
+}
+void line_detect(void)
+{
+/*   //keep on checking the sensor detecting white or not
+   while(1)
+   {
+      //white line detected 
+      if(!(*GPIOA_IDR & 1<<1))
+      {
+	 //a0 output  pin set as 0
+	 *GPIOA_ODR &= (0<<0);
+      }
+      else//white line not detect and black detected
+      {
+	 //ao output pin set as 1
+	 *GPIOA_ODR |= (1<<0);
+      }
+   }*/
+   while(1)
+   {
+      if(*GPIOA_IDR & (1<<1))
+      {
+	 *GPIOA_ODR |= (1<<0);
+      }
+      else
+      {
+	 *GPIOA_ODR &= (0<<0);
+      }
+   }
+}
+int main()
+{  
+   rcc_config();
+   gpio_config();
+   line_detect();
+}
